8257d7ac7c3ce3d5ae0aace4514ddf36bf358863
Brayan Gonzalez <brayangon.18@outlook.com>
2026-03-13 07:53:32 -0500

    ruta

--- app.py
+++ app.py
@@ -521,5 +521,8 @@ def cambiar_estado_pedido(id):
 # EJECUTAR APP
 # -------------------------
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
